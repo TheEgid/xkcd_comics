e9c969f2f39f7bd4e183556ee847c3bd31688709
TheEgid <40724661+TheEgid@users.noreply.github.com>
2019-02-25 10:56:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,10 +11,10 @@ def get_comics_total_qty():
 
 
 def get_random_comics_number(comics_total_qty):
-	if comics_total_qty is not None:
-		return random.randint(1, comics_total_qty)
-	else:
-		return None
+    if comics_total_qty is not None:
+       return random.randint(1, comics_total_qty)
+    else:
+       return None
 	
 r = get_comics_total_qty()
 print(get_random_comics_number(r))

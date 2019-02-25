5a25acb8a554776e267f178a09fca4185589eefe
TheEgid <40724661+TheEgid@users.noreply.github.com>
2019-02-25 10:43:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 import requests
+import random
 
-url = 'https://www.xkcd.com/info.0.json'
+def get_comics_total_qty():
+  url = 'https://www.xkcd.com/info.0.json'
+  response = requests.get(url=url)
+  if response.ok:
+    return response.json()['num']
+  else:
+    return None
 
-response = requests.get(url=url)
-if response.ok:
-  print(response.json()['num'])
+r = get_comics_total_qty()
+
+def get_random_comics_number(comics_total_qty):
+	if comics_total_qty is not None:
+		return random.randint(1, comics_total_qty)
+	else:
+		return None
+
+print(get_random_comics_number(r))
